e325ea7354e6d3e4f54fd2a24d6d5aad71d6bee0
Alejandra González <maljagonzalez01@gmail.com>
2019-11-29 15:22:07 -0500

    UML y ánalisis

---
+++ src/vista/Main.java
@@ -0,0 +1,24 @@
+package vista;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("vista.Main");
+
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+
+}
