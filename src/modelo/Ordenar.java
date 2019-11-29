08cdb753499f2d0dfa0533a75a787d1112c4784f
Alejandra González <maljagonzalez01@gmail.com>
2019-11-29 17:46:55 -0500

    odenamiento

--- src/modelo/Ordenar.java
+++ src/modelo/Ordenar.java
@@ -5,7 +5,14 @@ import java.util.Comparator;
 public class Ordenar implements Comparator<Personaje>{
 
 	public int compare(Personaje edad, Personaje edad1) {
+		if (edad.edad == edad1.edad) {     
+            return 0;
+        } else if (edad.edad > edad1.edad) {   
+        } else {                          
+            return -1;
+        }
 		return 0;
+	
 	}
 
 }
